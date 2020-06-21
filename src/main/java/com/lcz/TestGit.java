26efa3bb63bacaa65421332bcb04eb16889263f5
laosanli1 <14773997531@qq.com>
2020-06-21 09:23:46 +0800

    Initial commit

---
+++ src/main/java/com/lcz/TestGit.java
@@ -0,0 +1,16 @@
+package com.lcz;
+
+/**
+ * ClassName:TestGit
+ * Package:com.lcz
+ * Description:
+ *
+ * @Date:2020/6/21 9:19
+ * @Author:$licunzhuang@13253633031
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Test Git");
+    }
+
+}
